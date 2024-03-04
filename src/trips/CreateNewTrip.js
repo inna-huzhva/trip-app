3244f6e90c3cd380f656afadf7723a801267989f
Inna Huzhva <guzhva.inna@gmail.com>
2024-03-04 15:49:22 +0200

    Add form validation

--- src/trips/CreateNewTrip.js
+++ src/trips/CreateNewTrip.js
@@ -7,6 +7,21 @@ function CreateNewTrip({ closeModal, addTrip }) {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
 
+  const today = new Date().setUTCHours(0, 0, 0, 0);
+  const startTime = new Date(startDate).getTime();
+  const endTime = new Date(endDate).getTime();
+  const diffDays = Math.floor((endTime - startTime) / (1000 * 60 * 60 * 24));
+
+  const earlyStart = startTime < today ? "Start should not be in the past" : undefined;
+  const earlyEnd = endTime < startTime ? "End should be after start" : undefined;
+  const lateEnd = diffDays > 15 ? "End should be within 15 days from start" : undefined;
+
+  const startError = earlyStart;
+  const endError = earlyEnd || lateEnd;
+
+  const filled = city && startDate && endDate;
+  const disabled = !filled || startError || endError;
+
   return (
     <div className="modal-background">
       <div className="modal-container">
@@ -35,25 +50,29 @@ function CreateNewTrip({ closeModal, addTrip }) {
             <input 
               type="date"
               id="start"
-              value={ startDate }
+              className={startError ? "has-error" : ""}
+              value={startDate}
               onChange={e => setStartDate(e.target.value)}
               required
             />
+            {startError && <div className="error">{startError}</div>}
           </div>
           <div className="input-container">
             <label htmlFor="finish"><span className="asterisk">*</span> End date<br /></label>
             <input 
               type="date"
               id="finish"
-              value={ endDate }
+              className={endError ? "has-error" : ""}
+              value={endDate}
               onChange={e => setEndDate(e.target.value)}
               required
             />
+            {endError && <div className="error">{endError}</div>}
           </div>
         </div>
         <div className="modal-footer">
           <button className="cancel-btn" onClick={closeModal}>Cancel</button>
-          <button className="save-btn" onClick={() => {
+          <button className="save-btn" disabled={disabled} onClick={() => {
             const imgURL = cities.find(c => c.name === city).img;
             addTrip({city, startDate, endDate, imgURL});
             closeModal();
